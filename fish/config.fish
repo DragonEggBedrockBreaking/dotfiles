d14080ffd55602f1e3e97ae44cca1e5ee63c1ec1
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-07-29 22:25:19 +0100

    add dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+alias uplight="xbacklight -inc 10"
+alias downlight="xbacklight -dec 10"
+
+alias togglemute="pactl -- set-sink-mute @DEFAULT_SINK@ toggle"
+alias mute="pactl -- set-sink-mute @DEFAULT_SINK@ true"
+alias unmute="pactl -- set-sink-mute @DEFAULT_SINK@ false"
+
+alias upvolume="pactl set-sink-volume @DEFAULT_SINK@ +10%"
+alias downvolume="pactl set-sink-volume @DEFAULT_SINK@ -10%"
