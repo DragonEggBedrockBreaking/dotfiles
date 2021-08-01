6734cd6fd73d5acad2816815cbd3ec4bd9fe61c9
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-08-01 15:01:29 +0100

    change volume increments

--- fish/config.fish
+++ fish/config.fish
@@ -4,5 +4,5 @@ alias downlight="xbacklight -dec 10"
 alias togglemute="pactl -- set-sink-mute @DEFAULT_SINK@ toggle"
 alias togglemutemic="sh ~/.config/fish/scripts/pulseaudio/togglemic.sh"
 
-alias upvolume="pactl set-sink-volume @DEFAULT_SINK@ +10%"
-alias downvolume="pactl set-sink-volume @DEFAULT_SINK@ -10%"
+alias upvolume="pactl set-sink-volume @DEFAULT_SINK@ +5%"
+alias downvolume="pactl set-sink-volume @DEFAULT_SINK@ -5%"
