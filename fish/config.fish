f7b2a86d96569c64d8d282a6e7b9db6b06fa573a
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-09-26 15:59:21 +0100

    add most stuff to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
+alias brave='brave-browser-nightly --enable-features=UseOzonePlatform --ozone-platform=wayland'
 alias gimp='flatpak run org.gimp.GIMP//beta'
+
 alias cat='bat'
 alias ls='exa -a'
 alias find='find'
