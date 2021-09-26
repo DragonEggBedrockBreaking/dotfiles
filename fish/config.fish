fdfe122392fc2b446d7c34223e24632bedac65c2
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-09-26 16:32:28 +0100

    add more arguments to aliases, and create them for both standard and rust replacement commands

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,17 @@
 alias brave='brave-browser-nightly --enable-features=UseOzonePlatform --ozone-platform=wayland'
 alias gimp='flatpak run org.gimp.GIMP//beta'
 
-alias cat='bat'
-alias ls='exa -a'
-alias find='find'
-alias grep='rg'
-alias ps='procs'
+alias cat='bat --force-colorization --theme ansi'
+alias bat='bat --force-colorization --theme ansi'
+
+alias ls='exa -lah@ --colour=auto --icons --no-permissions --git'
+alias exa='exa -lah@ --colour=auto --icons --no-permissions --git'
+
+alias find='fd -uui -j 4'
+alias fd='fd -uui -j 4'
+
+alias grep='rg -uuudp -j 4'
+alias rg='rg -uuudp -j 4'
+
+alias ps='procs -t --color always --theme dark'
+alias procs='procs -t --color always --theme dark'
