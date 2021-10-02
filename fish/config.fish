2da1df3c17c78448f7cdc20ab90bb30a0efb34a9
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-10-02 14:54:39 +0100

    remove a lot of aliases so keep compatibility, add a bunch of aliases for mic volume with amixer
    (the speaker volume is configured through the qtile bar)

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,14 @@
 alias brave='brave-browser-nightly --enable-features=UseOzonePlatform --ozone-platform=wayland'
 alias gimp='flatpak run org.gimp.GIMP//beta'
 
-alias cat='bat --force-colorization --theme ansi'
 alias bat='bat --force-colorization --theme ansi'
-
-alias ls='exa -lah@ --colour=auto --icons --no-permissions --git --group-directories-first'
 alias exa='exa -lah@ --colour=auto --icons --no-permissions --git --group-directories-first'
-
-alias find='fd -uui -j 4'
+alias exatree='exa --tree --level=2 --colour=auto'
 alias fd='fd -uui -j 4'
-
-alias grep='rg -uuudp -j 4'
+alias procs='procs -t --color always --theme dark'
 alias rg='rg -uuudp -j 4'
 
-alias ps='procs -t --color always --theme dark'
-alias procs='procs -t --color always --theme dark'
-
-alias exatree='exa --tree --level=2 --colour=auto'
-alias tree='tree -a'
+alias mutemic='amixer set Capture cap'
+alias unmutemic='amixer set Capture nocap'
+alias incmic='amixer set Capture 1db+'
+alias decmic='amixer set Capture 1db-'
