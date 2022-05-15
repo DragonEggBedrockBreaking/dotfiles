7c08d4ad1a153e05e3c2fce10370549cea4d82d0
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-05-15 16:56:15 +0100

    Fix indents

--- nvim/init.lua
+++ nvim/init.lua
@@ -288,28 +288,28 @@ return require('packer').startup(function()
         'kyazdani42/nvim-tree.lua',
         requires = 'kyazdani42/nvim-web-devicons',
         config = function()
-			require('nvim-tree').setup {
-				disable_netrw = true,
-				hijack_cursor = true,
-				open_on_setup_file = true,
-				update_cwd = true,
-				view = {
-					width = 25,
-					height = 0
-				},
-				renderer = {
-					indent_markers = {
-						enable = true
-					},
-				},
-				diagnostics = {
-					enable = true,
-					show_on_dirs = true
-				},
-				git = {
-					ignore = false
-				}
-			}
+            require('nvim-tree').setup {
+                disable_netrw = true,
+                hijack_cursor = true,
+                open_on_setup_file = true,
+                update_cwd = true,
+                view = {
+                    width = 25,
+                    height = 0
+                },
+                renderer = {
+                    indent_markers = {
+                        enable = true
+                    },
+                },
+                diagnostics = {
+                    enable = true,
+                    show_on_dirs = true
+                },
+                git = {
+                    ignore = false
+                }
+            }
         end
     }
     use { -- visual indents
