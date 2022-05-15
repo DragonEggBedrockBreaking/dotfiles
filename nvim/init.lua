0a05fb74f138d9e1df79cf696b36b763fbfe771e
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-05-15 12:55:57 +0100

    Refactor nvim plugin list using 'requires', and remove some unneeded plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -421,31 +421,54 @@ require("nvim-tree").setup {
 -----------
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim' --plugin manager manages itself
-    use 'neovim/nvim-lspconfig' --language server protocol in neovim
-    use 'hrsh7th/cmp-nvim-lsp' --autocompletion
-    use 'hrsh7th/cmp-buffer' --autocompletion
-    use 'hrsh7th/nvim-cmp' --autocompletion
-    use { 'zbirenbaum/copilot-cmp', requires = 'zbirenbaum/copilot.lua' } --github copilot
-    use 'L3MON4D3/LuaSnip' --snippets
-    use 'saadparwaiz1/cmp_luasnip' --snippets
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } --better syntax highlighting
-    use 'mfussenegger/nvim-dap' --debugger
-    use 'mfussenegger/nvim-dap-python' --python debugger
+    use { -- autocompletion/snippets/copilot
+        'hrsh7th/nvim-cmp',
+        requires = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'neovim/nvim-lspconfig',
+            {
+                'zbirenbaum/copilot-cmp',
+                requires = 'zbirenbaum/copilot.lua'
+            },
+            {
+                'saadparwaiz1/cmp_luasnip',
+                requires = 'L3MON4D3/LuaSnip' 
+            }
+        }
+    }
+    use { -- better syntax highlighting
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate' 
+    }
+    use { -- debugging
+        'mfussenegger/nvim-dap',
+        requires = 'mfussenegger/nvim-dap-python'
+    }
+    use { -- the theme; make background transparent
+        'tanvirtin/monokai.nvim',
+        requires = 'xiyaowong/nvim-transparent'
+    }
+    use { -- fast fuzzy finder
+        'nvim-telescope/telescope.nvim',
+        requires = 'nvim-lua/plenary.nvim'
+    }
+    use { -- buffer line and bottom info line
+        'tamton-aquib/staline.nvim',
+        requires = {
+            'romgrk/barbar.nvim',
+            requires = 'kyazdani42/nvim-web-devicons'
+        }
+    }
+    use { -- file manager
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons'
+    }
     use 'ellisonleao/glow.nvim' --markdown previewer
-    use 'mg979/vim-visual-multi' --multiple cursors
     use 'lukas-reineke/indent-blankline.nvim' --visual indents
     use 'numToStr/Comment.nvim' --commenting in nvim
-    use 'tanvirtin/monokai.nvim' --theme
     use 'editorconfig/editorconfig-vim' --editorconfig
     use 'windwp/nvim-autopairs' --automatic pairs
-    use 'nvim-lua/popup.nvim' --common functions
-    use 'nvim-lua/plenary.nvim' --common functions
-    use 'nvim-telescope/telescope.nvim' --nvim telescope
-    use 'jvgrootveld/telescope-zoxide' --zoxide inside nvim telescope
-    use 'yamatsum/nvim-cursorline' --highlights where the cursor is, and the same keyword
-    use 'xiyaowong/nvim-transparent' --makes the background transparent
-    use { 'romgrk/barbar.nvim', requires = 'kyazdani42/nvim-web-devicons' } --bufferline
-    use 'tamton-aquib/staline.nvim' --tabline
+    use 'yamatsum/nvim-cursorline' --highlights where the cursor is
     use 'jghauser/mkdir.nvim' --auto mkdir
-    use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' } -- file manager
 end)
