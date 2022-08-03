36f012b9a486cf6ceae72d85bebd4d9f36a853bc
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-08-03 12:59:36 +0100

    Update nvim config

--- nvim/lua/plugins/coq.lua
+++ nvim/lua/plugins/coq.lua
@@ -1,4 +1,6 @@
 vim.g.coq_settings = {
     auto_start = 'shut-up'
 }
-require('lspconfig')['pylsp'].setup(require('coq').lsp_ensure_capabilities({}))
+require('lspconfig')['jedi_language_server'].setup(require('coq').lsp_ensure_capabilities({}))
+require('lspconfig')['clangd'].setup(require('coq').lsp_ensure_capabilities({}))
+require('lspconfig')['rust_analyzer'].setup(require('coq').lsp_ensure_capabilities({}))
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'json', 'json5', 'lua', 'python', 'vim' },
+    ensure_installed = { 'cpp', 'json', 'json5', 'lua', 'python', 'rust', 'vim' },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false
