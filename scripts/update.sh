5bfe584bd589ced39f478c9170fb083922c04fc7
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2023-02-13 18:53:31 +0000

    Safing Portmaster

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,9 +11,3 @@ sdk selfupdate                            # update sdkman
 sdk list java | rg amzn | rg "17\."           # List java versions
 
 nvim -c ":PackerSync" # update neovim plugins
-
-wget -nv https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts # download hosts file
-sudo rm /etc/hosts                                                                                                 # remove old hosts file
-sudo mv hosts /etc                                                                                                 # move new hosts file to its location
-sudo systemctl restart NetworkManager                                                                              # restart network manager to apply changes
-sed -n -e "6,8 p" -e "8 q" /etc/hosts                                                                              # give the user the date
