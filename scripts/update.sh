225b18caadc38028730532f86e81572a9ad566b6
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2023-07-21 11:12:48 +0100

    Change update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-paru -Syu                 # updates packages with paru
+update --skip-mirrorlist --aur # updates packages with pacman + paru, using garuda so that extra steps are completed (e.g. update grub after it updates)
 paru -Qdtq | paru -Rcns - # removes orphaned packages with paru
-
-rustup upgrade                            # update rust
+rustup upgrade # update rust
