87cc07d80be030dc763cae6a31cb9536ae13bd45
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2023-12-31 15:05:27 +0000

    Improve update script in terms of sdkman packages

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,15 +3,21 @@
 sudo apt update
 sudo apt upgrade
 sudo apt autoremove
+
 rustup upgrade
 cargo install-update -a
+
 sudo npm update -g
+
 sudo snap refresh
+
 flatpak update
+
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk selfupdate
 sdk update
-sdk list java | rg tem | rg ' 8'
-sdk list java | rg 'fx\-zulu' | rg 17
-sdk list java | rg 'graal ' | rg 21
+echo "Finding new sdkman package versions..."
+sdk list java | rg tem | rg ' 8' | rg -v 'installed'
+sdk list java | rg 'fx\-zulu' | rg 17 | rg -v 'installed'
+sdk list java | rg 'graal ' | rg 21 | rg -v 'installed'
