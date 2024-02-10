20411d3d983bd8d11db9118bdf6bdb0b58afe33f
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2024-02-10 10:14:30 +0000

    stuff

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,7 +22,7 @@ curl -s "https://get.sdkman.io" | bash                                          
 source "$HOME/.sdkman/bin/sdkman-init.sh"                                           # setup sdkman
 sdk install java 8.0.402-tem                                                        # used for older mc versions
 sdk install java 21.0.2-graal                                                       # used for newer mc versions
-sdk install java 17.0.9.fx-zulu                                                     # used for general development - default (latest sdk)
+sdk install java 17.0.10.fx-zulu                                                     # used for general development - default (latest sdk)
 # discord
 sudo -E gpg --no-default-keyring --keyring=/usr/share/keyrings/javinator9889-ppa-keyring.gpg --keyserver keyserver.ubuntu.com --recv-keys 08633B4AAAEB49FC
 sudo tee /etc/apt/sources.list.d/javinator9889-ppa.list <<< "deb [arch=amd64 signed-by=/usr/share/keyrings/javinator9889-ppa-keyring.gpg] https://ppa.javinator9889.com all main"
